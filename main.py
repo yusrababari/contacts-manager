daeb87fa6212b591c944fbaf9313421f29f50d4f
yusra <yusra.babari@outlook.com>
2026-06-19 23:36:58 +0100

    made changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# outputs json file which stores data
-import json
+
+import json  #outputs json file which stores data
+import jmespath #jmespath used to search through data
+
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-print(json.dumps(data, indent=4))
+print(json.dumps(data, indent=4))#outputs json file which stores data
