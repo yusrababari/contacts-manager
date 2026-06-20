27e621b714715dbe7f1f1835c81181bc5496b0be
yusrababari <yusra.babari@outlook.com>
2026-06-20 22:02:55 +0100

    made changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
-import json  #outputs json file which stores data
-import jmespath #jmespath used to search through data
+# https://medium.com/toyota-connected-india/a-gentle-introduction-to-jmespath-an-intuitive-way-to-parse-json-documents-daa6d699467a - json tutorial
+import json      # outputs json file which stores data
+import jmespath  # jmespath used to search through data
 
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-print(json.dumps(data, indent=4))#outputs json file which stores data
+results = jmespath.search('contacts[?age > `20`].[firstName, lastName]', data)
 
-print(jmespath.search('people[?age > `20`].{"name":name, "age": age}', data)) 
+names = [f"{first} {last}" for first, last in results]# loops through results and combines each first and last name into a single string
+
+print(names)
